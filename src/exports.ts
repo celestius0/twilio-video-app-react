042b1e1311a09abae0a3b385085c8f3bdc126d46
Ash Ramirez <aramirez@alucio.io>
2020-07-08 11:03:56 -0700

    re added twilio files

--- src/exports.ts
+++ src/exports.ts
@@ -1,6 +1,4 @@
 import Room from './components/Room/Room';
 import Controls from './components/Controls/Controls';
 
-const TestString = 'a test string';
-
-export { Room, Controls, TestString };
+export { Room, Controls };
